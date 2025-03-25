bcb7d5fe37ad2311d0d84a731726e449e044abac
Shree16112021 <sksmate05@gmail.com>
2025-03-25 12:00:12 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -3,6 +3,6 @@ resource "aws_instance" "main-instance" {
   instance_type = var.instance_type
   subnet_id     = aws_subnet.public-subnet.id
   tags = {
-    Name = "main-instance"
+    Name = "Master-instance"
   }
 }
